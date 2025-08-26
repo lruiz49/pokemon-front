1b07d12fee39dc05024867c62ddc79da1d956fb8
lruiz49 <lruiz1610@gmail.com>
2025-08-26 12:17:10 +0000

    feat: add TypePill component and color mappings for Pokémon types

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { PokemonCard } from './components/PokemonCard'
 import { FilterButton } from './components/ui/filter'
 import { PokeballIcon } from './components/ui/PokeballIcon'
 import { SearchBar } from './components/ui/SearchBar'
+import { TypePill } from './components/ui/TypePill'
 
 function App() {
 
@@ -26,11 +27,27 @@ function App() {
           </div>
         </div>
       </div>
-      <main className=' px-6 py-8'>
+      <main className=' px-6 '>
         <div className='text-xl font-semibold'>
           <h1>
-            <div className="text-xl font-semibold">
-              <h1>Cards</h1>
+            <div className="grid pt-4 pb-6 gap-2 [grid-template-columns:repeat(auto-fill,minmax(64px,1fr))]">
+              <TypePill type="bug" />
+              <TypePill type="dark" />
+              <TypePill type="dragon" />
+              <TypePill type="electric" />
+              <TypePill type="fairy" />
+              <TypePill type="fighting" />
+              <TypePill type="fire" />
+              <TypePill type="flying" />
+              <TypePill type="ghost" />
+              <TypePill type="ground" />
+              <TypePill type="ice" />
+              <TypePill type="normal" />
+              <TypePill type="poison" />
+              <TypePill type="psychic" />
+              <TypePill type="rock" />
+              <TypePill type="steel" />
+              <TypePill type="water" />
             </div>
             <div className="grid gap-6 [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))]">
               {items.map((item) => (
---
+++ src/components/ui/TypePill.tsx
@@ -0,0 +1,16 @@
+import { typeColors } from "@/lib/typeColors";
+
+export function TypePill({ type }: { type: string }) {
+  const color = typeColors[type] ?? "bg-gray-300";
+
+  return (
+    <div
+      className={`
+        flex items-center justify-center h-7 px-3 rounded-full 
+        text-xs font-semibold text-white capitalize  ${color}
+      `}
+    >
+      {type}
+    </div>
+  );
+}
